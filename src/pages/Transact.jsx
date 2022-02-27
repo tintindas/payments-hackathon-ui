d22d3a15bf58a8837f936ed7fae6a9d84eda2bd5
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-02-27 15:29:23 +0530

    add transact page

--- src/pages/Transact.jsx
+++ src/pages/Transact.jsx
@@ -2,17 +2,23 @@ import Card from '../components/Card'
 
 const Transact = () => {
 	return (
-		<div className='container payment'>
-			<h2>Payment</h2>
+		<div className='container transact'>
+			<div className='heading'>
+				<h2>Transact</h2>
+				<p>Choose your transaction type</p>
+			</div>
 			<form>
+				<label>
+					<p>Account Number:</p>
+					<input type='text' />
+				</label>
 				<label>
 					<p>Amount:</p>
 					<input type='number' />
 				</label>
 			</form>
 			<Card content={['Standard', '3g CO₂']} />
-			<Card content={['Immediate', '5g CO₂']} />
-			<Card content={['International', '12g CO₂']} />
+			<Card content={['CHAPS', '5g CO₂']} />
 		</div>
 	)
 }
